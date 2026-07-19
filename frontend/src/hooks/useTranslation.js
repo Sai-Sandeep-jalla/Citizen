38f0c5391dccc4a03c8b702199720d33c04f6489
Sai Sandeep <jallasaisandeep2003@gmail.com>
2026-07-19 18:20:33 +0530

    Remove all files from main

--- frontend/src/hooks/useTranslation.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { LanguageContext } from "../context/LanguageContext";
-
-export default function useTranslation() {
-
-    return useContext(LanguageContext);
-
-}
